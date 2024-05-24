2f6098a705708e69d8fcf1fab6182facc281938f
avery <avery@MELWMGNTB15136.localdomain>
2024-05-24 19:36:39 +1000

    fix some css problems

--- src/powerblog/layout.clj
+++ src/powerblog/layout.clj
@@ -1,16 +1,15 @@
 (ns powerblog.layout)
 
-
 (def header
   [:header.prose.max-w-screen.mx-auto.pb-6
-   [:nav.flex.items-center.justify-between.border-b.border-gentle-black.max-h-12.relative
+   [:nav.bg-hl-sepia.flex.items-center.justify-between.border-b.border-gentle-black.max-h-12.relative
     [:ul.list-none.pl-0
      [:li
       [:a {:href "/"} "home"]]]
     [:ul.list-none.pl-0.hidden.lg:block
      [:li
       [:a {:href "/"} "programming resources"]]]
-    [:ul.list-none.pl-0
+    [:ul.list-none.pl-0.pr-2
      [:li
       [:a {:href "/about/"} "about"]]]]])
 
